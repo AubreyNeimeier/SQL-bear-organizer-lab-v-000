81f85b9c9e4bcd0a3df45ff0155eaed757731ff5
Aubrey Neimeier <neimeier.1@osu.edu>
2018-02-27 15:34:08 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,9 @@
-INSERT INTO bears(name, age, gender, color, temperment,alive) VALUES ("Mr.Chocolate", 12, "F", "black", "fiesty", 1)
+INSERT INTO bears(name, age, gender, color, temperment,alive) VALUES ("Mr.Chocolate", 12, "F", "black", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperment,alive) VALUES ("Rowdy", 12, "F", "black", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperment,alive) VALUES ("Tabitha", 12, "F", "black", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperment,alive) VALUES ("Sergeant Brown", 12, "F", "black", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperment,alive) VALUES ("Melissa", 12, "F", "black", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperment,alive) VALUES ("Grinch", 12, "F", "black", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperment,alive) VALUES ("Wendy", 12, "F", "black", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperment,alive) VALUES (NULL, 12, "F", "black", "fiesty", 1);
+
