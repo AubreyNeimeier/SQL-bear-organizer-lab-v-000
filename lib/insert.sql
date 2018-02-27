91135a7e454d18c29a709e4a8a8ad72ebe4ceca3
Aubrey Neimeier <neimeier.1@osu.edu>
2018-02-27 15:37:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12, "F", "black", "fiesty", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "F", "black", "fiesty", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 12, "F", "black", "fiesty", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "F", "black", "fiesty", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "F", "black", "fiesty", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "F", "black", "fiesty", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "F", "black", "fiesty", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 12, "F", "black", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12, "F", "brown", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 24, "F", "black", "fiesty", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "black", "fiesty", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9, "F", "black", "fiesty", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "M", "red", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 20, "M", "brown", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "F", "black", "fiesty", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 41, "F", "black", "fiesty", 1);
